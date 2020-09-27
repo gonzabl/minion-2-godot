b3ff4eb7fbdb256e0a8aa4cc9dff742f4bc84b36
gonzabl <gonzabl@live.com.ar>
2020-09-27 17:28:04 -0300

    violeta se mueve y salta

---
+++ Violeta.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+const FLOOR_NORMAL = Vector2.UP
+
+var speed = 300
+var jump_speed = -350
+var gravity = 2000
+
+var velocity = Vector2()
+
+func get_input():
+	velocity.x = 0
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+	
+	
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity,FLOOR_NORMAL)
+	
